1ae493915d8f3bac44fdd2107db1bb6f746d0f0e
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-07-02 21:04:44 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+// This widget is
+//the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ClipOval',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePAGE(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePAGE extends StatefulWidget {
+  @override
+  _MyHomePAGEState createState() => _MyHomePAGEState();
+}
+
+class _MyHomePAGEState extends State<MyHomePAGE> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GeeksforGeeks'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+          child: Image.network(
+              'https://raw.githubusercontent.com/flutter/assets-for-api-docs/master/packages/diagrams/assets/blend_mode_destination.jpeg',
+              color: Color.fromRGBO(255, 255, 255, 0.5),
+              colorBlendMode: BlendMode.modulate
+          )
+      ),
+      backgroundColor: Colors.lightBlue[50],
+    );
+  }
+}
+
+class MyClip extends CustomClipper<Rect> {
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, 100, 100);
+  }
+
+  bool shouldReclip(oldClipper) {
+    return false;
+  }
+}
